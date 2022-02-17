8aa741b1ab5e025a840bf7292a7e9fcc678a6253
RabiyaRafeeq <50367671+RabiyaRafeeq@users.noreply.github.com>
2022-02-17 16:12:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app=express()
 const path=require('path')
 const router=express.Router()
 router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'))
+    res.sendFile(path.join(__dirname,'views','/index.html'))
 })
 router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/about.html'))
+    res.sendFile(path.join(__dirname,'views''/about.html'))
 })
 app.use('/',router)
 app.listen(3000)
